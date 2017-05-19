b5d49a469ee802efd76ef501bc43dcd55f4cfe70
NicoProjet <nicolas.feron@ulb.ac.be>
2017-05-19 15:03:57 +0200

    a

--- src/museum/WatchMuseum.java
+++ src/museum/WatchMuseum.java
@@ -7,8 +7,14 @@ public class WatchMuseum {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		ArrayList<String> map = Parser.parse(args[0]);
-
+		ArrayList<String> map;
+		if (args[0] != null){
+			map = Parser.parse(args[0]);
+		}
+		else{
+			map = Parser.parse("map.txt");
+		}
+		
 	}
 
 }
