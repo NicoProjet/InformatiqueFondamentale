9b2ce07044414a034043470a973f5bc72214be6e
fishy <knishio@ulb.ac.be>
2017-05-19 14:51:58 +0200

    lala

--- src/museum/Parser.java
+++ src/museum/Parser.java
@@ -1,5 +1,24 @@
 package museum;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class Parser {
+
+    static public ArrayList<String> parse(String file) throws IOException {
+        BufferedReader in = null;
+        ArrayList<String> map = new ArrayList<String>();
+        String currentLine;
+        try {
+            in = new BufferedReader(new FileReader(file));
+            while((currentLine = in.readLine()) != null) map.add(currentLine);
+        }finally {
+            if(in != null) in.close();
+
+        }
+        return map;
+    }
 
 }
--- src/museum/WatchMuseum.java
+++ src/museum/WatchMuseum.java
@@ -1,9 +1,13 @@
 package museum;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class WatchMuseum {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		ArrayList<String> map = Parser.parse(args[1]);
 
 	}
 
